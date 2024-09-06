f6a3220efc12416cb1a2285601bf603324eef0c0
Eduarda_Cardoso <deveduardacardoso@gmail.com>
2024-09-06 10:19:22 -0300

    Arrumando as classes

--- src/BitCounting.java
+++ src/BitCounting.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BitCounting {
+public class BitCounting {
+    /*
+    Write a function that takes an integer as input,
+    and returns the number of bits that are equal to
+    one in the binary representation of that number.
+    You can guarantee that input is non-negative.
+
+    Example: The binary representation of 1234 is 10011010010
+    so the function should return 5 in this case
+     */
+    public static int countBits(int n){
+        int count= 0;
+
+        for(int i =0; n>=1; i++){
+            if(n%2==1){
+                count++;
+            }
+            n = n/2;
+        }
+        return count;
+    }
 }
--- src/Twosum.java
+++ src/Twosum.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Twosum {
+public class Twosum {
+        /*
+        Write a function that takes an array of numbers (integers for the tests) and a target number.
+        It should find two different items in the array that, when added together, give the target value.
+        The indices of these items should then be returned in a tuple / list (depending on your language)
+        like so: (index1, index2).
+
+        For the purposes of this kata, some tests may have multiple answers;
+        any valid solutions will be accepted.
+
+        The input will always be valid:
+        * numbers will be an array of length 2 or greater,
+        * all items will be numbers;
+        * target will always be the sum of two different items from that array).
+
+         */
+        public static int[] twoSum(int[] numbers, int target) {
+            int[] indice = new int[2];
+
+            for(int i=0;i<numbers.length;i++){
+                for(int j=numbers.length-1; j>i;j--){
+                    if(numbers[i]+numbers[j]==target){
+                        indice[0]=i;
+                        indice[1]=j;
+                    }
+                }
+            }
+            return indice;
+        }
 }
